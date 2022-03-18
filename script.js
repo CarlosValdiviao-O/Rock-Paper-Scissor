aa62342617e0daeb9884db61f7242da768fbba4a
CarlosValdiviao-O <carlosvaldivia390@gmail.com>
2022-03-18 10:46:50 -0400

    playRound function added

--- script.js
+++ script.js
@@ -1,17 +1,51 @@
 function computerPlay() {
     let number = Math.floor(Math.random() * 10);
     let choice;
-    for ( let i=0;number==0;i++){
-        i--;
+
+    //if random number is 0 roll again, so we only get numbers 1-9
+    while (number==0) {
+        number = Math.floor(Math.random() * 10);
     }
-    if (number==1 || number==4 || number==7) {
-        choice='Rock';
+
+    if (number%3==0) {
+        choice='rock';
     }
-    else if (number==2 || number==5 || number==8){
-        choice='Paper';
+    else if (number%3==1){
+        choice='paper';
     }
     else {
-        choice="Scissor";
-    }
+        choice="scissors";
+    } 
     return choice;
+}
+
+function playRound (playerSelection, computerSelection) {
+    console.log(`The computer draw: ` + computerSelection.substr(0,1).toUpperCase() + computerSelection.substr(1));
+    if (playerSelection==computerSelection){
+        console.log("Tie!");
+    }
+    else if (playerSelection=='rock'){
+        if (computerSelection=='paper'){
+            console.log("You lose! Paper beats rock");
+        }
+        else {
+            console.log("You win! Rock beats scissors");
+        }
+    }
+    else if (playerSelection=='paper'){
+        if (computerSelection=='scissors'){
+            console.log("You lose! Scissors beats paper");
+        }
+        else {
+            console.log("You win! Paper beats rock");
+        }
+    }
+    else {
+        if (computerSelection=='rock'){
+            console.log("You lose! Rock beats scissors");
+        }
+        else {
+            console.log("You win! Scissors beats paper");
+        }
+    }
 }
